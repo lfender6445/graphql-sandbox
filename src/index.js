587edea8e0d6495bf9173b4e69f404f033f09227
Luke Fender <lfender6445@gmail.com>
2019-02-14 20:43:11 -0500

    query with link / client setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-console.log('running')
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createStore } from 'redux'
 import fetch from 'node-fetch'
 
 global.fetch = fetch
 
 function counter(state = 0, action) {
-  console.log('action', action)
   switch (action.type) {
     case 'INCREMENT':
       return state + 1
@@ -18,7 +17,7 @@ function counter(state = 0, action) {
 
 let store = createStore(counter)
 
-store.subscribe(() => console.log(store.getState()))
+// store.subscribe(() => console.log(store.getState()))
 
 // The only way to mutate the internal state is to dispatch an action.
 // The actions can be serialized, logged or stored and later replayed.
@@ -33,7 +32,6 @@ import { parse } from 'graphql'
 
 import { execute } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-console.log({ HttpLink })
 import { renderToString } from 'react-dom/server'
 
 const express = require('express')
@@ -55,33 +53,32 @@ app.use('/graphql', graphqlHTTP(() => ({
 app.listen(0, function() {
   const port = this.address().port
   const uri = `http://localhost:${port}/graphql`
-  console.log(`Started on ${uri}`)
+
+  console.info(`Started on ${uri}`)
 
   const opts = {
     uri: uri,
     fetch: fetch,
   }
-  console.log({ opts })
+
   const link = new HttpLink(opts)
-  console.log('Made it here')
 
   const client = new ApolloClient({
     link,
-    cache: {},
+    cache: new InMemoryCache(),
   });
 
+  const query = gql`query {
+    test {
+      id
+    }
+  }`
 
-
-  // const query = gql`query {
-  //   id
-  // }`
-
-  // client.query({
-  //   query,
-  // }).then(data => {
-  //   console.log({ data })
-
-  // })
+  client.query({
+    query,
+  }).then(res => {
+    console.log({ data: res.data.test.id })
+  })
 })
 
 console.log('running')
